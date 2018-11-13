4715202da1744c07a5bc97f9c4e75446794a7cb1
Nikolas9896 <nhlynka@gmail.com>
2018-11-13 15:05:05 +0200

    added n limit from 1 to 1000 000 000

--- Matches_Task1_HlynkaMykola.java
+++ Matches_Task1_HlynkaMykola.java
@@ -12,3 +12,25 @@ Input example #1
 Output example #1
 12 
 */
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+
+	{
+		int squares = 0, matches = 0, one1 = 1, billion = 1000000000;
+	    
+	    System.out.println("***Task 1: Matches***");
+	    
+	    while ( squares < one1 || squares > billion )
+	        {
+	      
+	         System.out.println("Input number of squares:");
+	         Scanner S=new Scanner(System.in);
+             squares=S.nextInt();
+        
+        
+	        }
+	     System.out.println(" The minimum number of matches required to construct " + squares + " squares: " + matches);
+	}
+}
