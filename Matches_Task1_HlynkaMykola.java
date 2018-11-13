2a41d5cb81fd29bc98a9cfd8fd22dfe2dca51338
Nikolas9896 <nhlynka@gmail.com>
2018-11-13 16:06:59 +0200

    Ready on Java :+1:

--- Matches_Task1_HlynkaMykola.java
+++ Matches_Task1_HlynkaMykola.java
@@ -1,9 +1,13 @@
 /*
 **********************************Task 1: Matches***********************
-What is the minimum number of matches necessary to pit on the plane n squares with a side in one match? Matches can not be broken and put on each other. The vertices of the squares should be the points where the ends of the matches meet, and the sides are matches themselves.
+What is the minimum number of matches necessary to pit on the plane n squares with a side in one match? 
+Matches can not be broken and put on each other.
+The vertices of the squares should be the points where the ends of the matches meet, and the sides are matches themselves.
 Write a program that by the number of squares n to be constructed, finds the minimum number of matches required for this.
+
 Input
 One integer n (1 ≤ n ≤ pow( 10, 9) ).
+
 Output
 Print the minimum number of matches required to construct n squares.
 
